6509cd7feef3863b250e68556f9f10d6de1b4be0
Carlos Florez <carlos.andres.ff2912@gmail.com>
2024-07-11 10:24:35 -0500

    Add switch component

---
+++ src/components/Shared/Header/SwitchToggle/index.tsx
@@ -0,0 +1,21 @@
+"use client"
+import React, { useState } from 'react'
+import { MoonIcon, MoonFillIcon } from '@/components/Shared/Icons'
+
+export const SwitchToggle = () => {
+  const [checked, setChecked] = useState(false)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(e.target.checked)
+  }
+
+  return (
+    <label className="inline-flex items-center cursor-pointer">
+      <input onChange={handleChange} type="checkbox" checked={checked} className="sr-only peer" />
+      <div className="relative w-9 h-5 bg-gray-500 rounded-full dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-purple-500" />
+      <span className="ms-2 text-sm font-medium text-gray-500 dark:text-gray-300">
+        {!checked ? <MoonIcon /> : <MoonFillIcon />}
+      </span>
+    </label>
+  )
+}
