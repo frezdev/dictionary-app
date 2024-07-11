4953b60d7ecf3f12bcb2f90efcb84bbfc3a4d840
Carlos Florez <carlos.andres.ff2912@gmail.com>
2024-07-11 10:50:57 -0500

    Add view of external sources - This is to show the url's of the sources.

---
+++ src/components/Home/Result/SourceUrls/index.tsx
@@ -0,0 +1,22 @@
+import { ExternalLinkIcon } from '@/components/Shared/Icons';
+
+interface Props {
+  sourceUrls: string[];
+}
+export const SourceUrls = ({ sourceUrls }: Props) => {
+  return (
+    <section className="flex flex-wrap gap-5 text-sm py-8">
+      <span className="text-gray-600">Source</span>
+      <ul className="grid gap-2">
+        {sourceUrls.map(url => (
+          <li key={url} className="w-full">
+            <a className="underline gap-1 items-center flex text-gray-600" href={url} target="_blank">
+              <span className='line-clamp-1 max-w-[80vw] break-words'>{url}</span>
+              <ExternalLinkIcon width={'1em'} height={'1em'} />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
