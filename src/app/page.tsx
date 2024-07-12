cc6e3694db6432688a26662b814ca0fa69483fb2
Carlos Florez <carlos.andres.ff2912@gmail.com>
2024-07-12 01:49:02 -0500

    Update Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import { MainSection } from "@/sections/Home/MainSection";
 import { Search } from "@/sections/Home/Search";
-import { Result } from "@/sections/Home/Result";
+
 
 export default function Home() {
   return (
     <main className="grid pb-8 sm:mt-3">
       <Search />
-      <Result />
+      <MainSection />
     </main>
   );
 }
