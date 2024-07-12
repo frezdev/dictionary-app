19d7726e0e2218d284e14e35eed6867879d77bfb
Carlos Florez <carlos.andres.ff2912@gmail.com>
2024-07-12 01:48:13 -0500

    Feat: Add service to get a word from the api

---
+++ src/services/words.ts
@@ -0,0 +1,19 @@
+import { APIResponse } from "@/types";
+
+const API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en'
+
+export class WordServices {
+  static async getByQuery(query: string): Promise<[Error | null, APIResponse[] | null]> {
+    try {
+      const res = await fetch(`${API_URL}/${query}`)
+
+      if (res.status !== 200) throw new Error(res.statusText);
+
+      const data = await res.json() as APIResponse[]
+      return [null, data]
+    } catch (error) {
+      if (error instanceof Error) return [new Error(error.message), null]
+      return [new Error('Unknown Error'), null]
+    }
+  }
+}
